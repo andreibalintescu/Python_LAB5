db35665b931d3250d1cc195bcd3ba9d45d812714
Balintescu <andrei.balintescu@stud.ubbcluj.ro>
2023-12-04 20:42:43 +0200

    Introduced data classes.

---
+++ Modelle/CUSTOMERS_AND_ORDERS/CUSTOMERS & ORDERS.py
@@ -0,0 +1,22 @@
+from Modelle.IDS.ID import Identifiable
+import functools
+class Customer(Identifiable):
+    def __init__(self,id,name,address):
+        Identifiable.__init__(self,id)
+        self.name = name
+        self.address = address
+
+class Order(Identifiable):
+    def __init__(self, id, customer_id, dishes_id_list, drinks_id_list, total_price):
+        Identifiable.__init__(self, id)
+        self.customer_id = customer_id
+        self.dishes_id_list = dishes_id_list
+        self.drinks_id_list = drinks_id_list
+        self.total_price = total_price
+
+    def calculate_total_price(self):
+        pass
+    def __generate_invoice(self):
+        pass
+    def print_invoice(self):
+        pass
---
+++ Modelle/DISHES/FOOD & BEVERAGE.py
@@ -0,0 +1,16 @@
+from Modelle.IDS.ID import Identifiable
+class Dish(Identifiable):
+    def __init__(self,id,weight,price):
+        Identifiable.__init__(self,id)
+        self.weight = weight
+        self.price = price
+
+class Cooked_Dish(Dish):
+    def __init__(self,id,weight,price,cooking_time):
+        Dish.__init__(self,id,weight,price)
+        self.cooking_time = cooking_time
+
+class Beverage(Dish):
+    def __init__(self,id,weight,price,alcohol_content):
+        Dish.__init__(self,id,weight,price)
+        self.alcohol_content = alcohol_content
